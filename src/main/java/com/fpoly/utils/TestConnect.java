c8a8e47a5271ae6fe63778d1239ce2d0a15a3b5a
iamducthanh <ducthanh260801@gmail.com>
2021-07-09 20:40:58 +0700

    fix

---
+++ src/main/java/com/fpoly/utils/TestConnect.java
@@ -0,0 +1,28 @@
+package com.fpoly.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class TestConnect {
+    public static void main(String[] args) {
+        try {
+            String DB_URL = "jdbc:mysql://us-cdbr-east-04.cleardb.com:3306/heroku_458d0af2410d609?reconnect=true";
+            String USER_NAME = "b92b48bf4d123b";
+            String PASSWORD = "5a4e760e";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            PreparedStatement ps = conn.prepareStatement("select * from users");
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1));
+                System.out.println(resultSet.getString(2));
+                System.out.println(resultSet.getString(3));
+                System.out.println(resultSet.getString(4));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
